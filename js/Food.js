1bf5a92eacf735737a412d73d28e208f07997cbb
gokeen10 <70183453+gokeen10@users.noreply.github.com>
2020-12-21 20:46:54 -0500

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,48 @@
+class Food {
+    constructor() {
+        this.image = loadImage("images/Milk.png");
+        this.foodStock = 0;
+        this.lastFed;
+    }
+    display() {
+        /*fill(255, 255, 254);
+        textSize(15);
+        if (lastFed>=12){
+            text("Last Feed : "+ lastFed%12 + " PM", 350, 30);
+       } else if(lastFed==0) {
+            text("Last Feed : 12 AM", 350, 30);
+        } else {
+            text("Last Feed : "+ lastFed + " AM", 350, 30);
+        } */
+        var x=80;
+        var y=100;
+        imageMode(CENTER);
+        image(this.image, 720, 220, 70, 70);
+        console.log(this.foodStock);
+        if(this.foodStock!==0){
+            for(var i=0; i<this.foodStock; i++) {
+                if(i%10===0){
+                    x=80;
+                    y=y+50;
+                }
+                image(this.image, x, y, 50, 50);
+                x=x+30;
+            }
+        }
+    } 
+    getFoodStock() {
+        return this.foodStock;
+    }
+    updateFoodStock(foodStock) {
+        this.foodStock=foodStock;
+    }
+    deductFood() {
+        if(this.foodStock>0){
+            this.foodStock=this.foodStock-1;
+        }
+    }
+    getFedTime(lastFed){
+        this.lastFed=lastFed;
+    }
+}
+
